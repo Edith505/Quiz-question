882ebc0232b3b5c125bfd855bfc7bcc8e275d12b
Edith505 <edithrand505@gmail.com>
2023-03-28 20:31:45 +0300

    question and answer

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+const question = [
+    {
+        question: "Quel language pour les interactions d'une page web?",
+        answoers :[
+            {text: "htlm", correct: false},
+            {text: "javascript", correct: true},
+            {text: "css", correct: false},
+            {text: "php", correct: false},
+        ]
+    },
+    {
+        question: "quel language pour une application android",
+        answoers :[
+            {text: "Laravel", correct: false},
+            {text: "React", correct: false},
+            {text: "Python", correct: false},
+            {text: "Java", correct: true},
+        ]
+    }
+];
+const questionElement = document.getElementById("question");
